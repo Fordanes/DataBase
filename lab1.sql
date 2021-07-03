aa13b29a760d11fa34e58488723f4e8a0ae9183c
Alan Khassanov <78968523+Fordanes@users.noreply.github.com>
2021-07-03 22:27:50 +0600

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,40 @@
+--Task 1
+create database lab1;
+
+--Task 2
+create table users(
+    id serial,
+    firstname varchar(50),
+    lastname varchar(50)
+);
+
+--Task 3
+alter table users
+add column isadmin int;
+
+--Task 4
+alter table users
+alter column isadmin type boolean using users.isadmin::boolean;
+
+--Task 5
+alter table users
+alter column isadmin set default false;
+
+--Task 6
+alter table users
+add constraint id primary key (id);
+
+--Task 7
+create table tasks(
+    id serial,
+    name varchar(50),
+    user_id integer
+);
+
+--Task 8
+drop table tasks;
+
+--Task 9
+drop database lab1;
+
+
